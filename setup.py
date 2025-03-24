40dd90dbe6c289b9e81dcfc5892d870b8e1d191d
Le ConTesteur <47255423+LeConTesteur@users.noreply.github.com>
2025-03-24 20:47:27 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+KEYWORDS = ('')
+DESCRIPTION = ''
+CLASSIFIERS = []
+
+requirements = [
+]
+
+requirements_tests = [
+]
+
+extras = {
+    'tests': requirements_tests,
+}
+
+NAME = 'python-template'
+MODULE = NAME.replace("-", "_")
+setuptools.setup(
+    name=NAME,
+    version="0.0.1",
+    author="LeConTesteur",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/LeConTesteur/{NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/LeConTesteur/{NAME}/issues",
+    },
+    classifiers=CLASSIFIERS,
+    package_dir={NAME: MODULE},
+    packages={"": MODULE},
+    python_requires=">=3.8",
+    install_requires = requirements,
+    tests_require = requirements_tests,
+    extras_require = extras,
+    keywords=KEYWORDS,
+)
